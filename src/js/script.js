9af0a136468a96b1b0486ba2aa7edc1ced09d785
Ivan <yadov126@gmail.com>
2023-09-02 13:20:05 +1000

    Pre-Final version

--- src/js/script.js
+++ src/js/script.js
@@ -53,4 +53,33 @@ $(document).ready(function(){
     });
 
     $('.telephone').mask("+7 (999) 999-9999")
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 1600){
+            $('.page-up').fadeIn();
+        } else{
+            $('.page-up').fadeOut();
+        }
+    });
+
+    $("a[href^=#up]").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 });
